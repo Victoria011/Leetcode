711b3d11a5df130ef1a27fbdfaecbf5b3fce702d
Victoria Lin <victoria.ry.lin@gmail.com>
2021-08-25 11:58:13 -0700

    LC148

--- src/LC147InsertionSortList/Solution.java
+++ src/LC147InsertionSortList/Solution.java
@@ -1,5 +1,23 @@
 package LC147InsertionSortList;
 
-public class Solution {
+import template.ListNode;
 
+public class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        ListNode dummy = new ListNode();
+        ListNode curr = head;
+        
+        while (curr != null) {
+            ListNode prev = dummy;
+            while (prev.next != null && prev.next.val < curr.val) {
+                prev = prev.next;
+            }
+            ListNode next = curr.next;
+            curr.next = prev.next;
+            prev.next = curr;
+            
+            curr = next;
+        }
+        return dummy.next;
+    }
 }
