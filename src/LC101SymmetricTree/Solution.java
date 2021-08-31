2f79b31ddde4bfd1bbeec870295e95f7eb63d3c1
Victoria Lin <victoria.ry.lin@gmail.com>
2021-08-31 13:58:19 -0700

    LC101

--- src/LC101SymmetricTree/Solution.java
+++ src/LC101SymmetricTree/Solution.java
@@ -1,16 +1,49 @@
 package LC101SymmetricTree;
 
+
 import template.TreeNode;
 
+
+
 // Approach 1 recursion
+//public class Solution {
+//    public boolean isSymmetric(TreeNode root) {
+//        return root == null || helper(root.left, root.right);
+//    }
+//    
+//    private boolean helper(TreeNode left, TreeNode right){
+//        if (left == null || right == null) return (left == right);
+//        if (left.val != right.val) return false;
+//        return helper(left.left, right.right) && helper(left.right, right.left);
+//    }
+//}
+
+//Approach 2 Iteration (why use Queue/Stack)
+
+import java.util.Queue;
+import java.util.LinkedList;
+
 public class Solution {
-    public boolean isSymmetric(TreeNode root) {
-        return root == null || helper(root.left, root.right);
-    }
-    
-    private boolean helper(TreeNode left, TreeNode right){
-        if (left == null || right == null) return (left == right);
-        if (left.val != right.val) return false;
-        return helper(left.left, right.right) && helper(left.right, right.left);
-    }
+ public boolean isSymmetric(TreeNode root) {
+     if (root == null) return true;
+     Queue<TreeNode> q = new LinkedList();
+     
+     q.add(root.left);
+     q.add(root.right);
+     while(!q.isEmpty()){
+         TreeNode left = q.poll();
+         TreeNode right = q.poll();
+         
+         if (left == null && right == null) continue;
+         if (left == null || right == null || left.val != right.val) return false;
+         
+         q.add(left.left);
+         q.add(right.right);
+         q.add(left.right);
+         q.add(right.left);
+         
+     }
+     
+     return true;
+ }
 }
