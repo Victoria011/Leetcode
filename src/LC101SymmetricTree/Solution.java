defa3e34b627d2eab11585e368db1eee4b302289
Victoria Lin <victoria.ry.lin@gmail.com>
2021-08-31 10:41:07 -0700

    LC101

---
+++ src/LC101SymmetricTree/Solution.java
@@ -0,0 +1,16 @@
+package LC101SymmetricTree;
+
+import template.TreeNode;
+
+// Approach 1 recursion
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return root == null || helper(root.left, root.right);
+    }
+    
+    private boolean helper(TreeNode left, TreeNode right){
+        if (left == null || right == null) return (left == right);
+        if (left.val != right.val) return false;
+        return helper(left.left, right.right) && helper(left.right, right.left);
+    }
+}
