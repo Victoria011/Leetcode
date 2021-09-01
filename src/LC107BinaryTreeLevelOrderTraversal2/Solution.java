982c3ce11487de078c7855a73bed851d63ad3276
Victoria Lin <victoria.ry.lin@gmail.com>
2021-09-01 10:01:18 -0700

    LC107

--- src/LC107BinaryTreeLevelOrderTraversal2/Solution.java
+++ src/LC107BinaryTreeLevelOrderTraversal2/Solution.java
@@ -25,3 +25,31 @@ public class Solution {
      return levels;
  }
 }
+
+//Approach 2: Iteration: BFS Traversal
+//class Solution {
+//  public List<List<Integer>> levelOrderBottom(TreeNode root) {
+//      List<List<Integer>> levels = new ArrayList<List<Integer>>();
+//      if (root == null) return levels;
+//      
+//      ArrayDeque<TreeNode> nextLevel = new ArrayDeque(){{offer(root);}};
+//      ArrayDeque<TreeNode> currLevel = new ArrayDeque();
+//      
+//      while(!nextLevel.isEmpty()) {
+//          currLevel = nextLevel.clone();
+//          nextLevel.clear();
+//          levels.add(new ArrayList<Integer>());
+//          
+//          for (TreeNode node: currLevel) {
+//              levels.get(levels.size()-1).add(node.val);
+//              
+//              if (node.left != null) nextLevel.offer(node.left);
+//              if (node.right != null) nextLevel.offer(node.right);
+//              
+//          }
+//      }
+//      
+//      Collections.reverse(levels);
+//      return levels;
+//  }
+//}
