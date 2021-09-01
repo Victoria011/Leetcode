087da5a317c7dd5032d4ef59b4461b435f4a6bd2
Victoria Lin <victoria.ry.lin@gmail.com>
2021-09-01 11:47:17 -0700

    LC111

--- src/LC111MinimumDepthofBinaryTree/Solution.java
+++ src/LC111MinimumDepthofBinaryTree/Solution.java
@@ -16,3 +16,39 @@ public class Solution {
         return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
     }
 }
+// 精简一点
+//int min_depth = Integer.MAX_VALUE;
+//if (root.left != null) {
+//  min_depth = Math.min(minDepth(root.left), min_depth);
+//}
+//if (root.right != null) {
+//  min_depth = Math.min(minDepth(root.right), min_depth);
+//}
+//
+//return min_depth + 1;
+
+// Approach 2 DFS iteration
+//class Solution {
+//    public int minDepth(TreeNode root) {
+//        LinkedList<Pair<TreeNode, Integer>> stack = new LinkedList<>();
+//        if (root == null) return 0;
+//        stack.add(new Pair(root,1));
+//        
+//        int min_depth = Integer.MAX_VALUE;
+//        while(!stack.isEmpty()) {
+//            Pair<TreeNode, Integer> current = stack.pollLast();
+//            root = current.getKey();
+//            int current_depth = current.getValue();
+//            if((root.left == null) && (root.right == null)) {
+//                min_depth = Math.min(min_depth, current_depth);
+//            }
+//            if (root.left != null){
+//                stack.add(new Pair(root.left, current_depth + 1));
+//            }
+//            if (root.right != null) {
+//                stack.add(new Pair(root.right, current_depth + 1));
+//            }
+//        }
+//        return min_depth;
+//    }
+//}
