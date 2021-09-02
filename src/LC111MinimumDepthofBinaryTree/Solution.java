bde98d18a4897d9412e1c742de6672c0f130d4d2
Victoria Lin <victoria.ry.lin@gmail.com>
2021-09-01 19:16:20 -0700

    LC111

--- src/LC111MinimumDepthofBinaryTree/Solution.java
+++ src/LC111MinimumDepthofBinaryTree/Solution.java
@@ -1,21 +1,21 @@
 package LC111MinimumDepthofBinaryTree;
 
 import template.TreeNode;
-
-public class Solution {
-    public int minDepth(TreeNode root) {
-        if (root == null) return 0;
-        if ((root.left == null) && (root.right == null)) return 1;
-        if ((root.left == null) || (root.right == null)) {
-            if (root.left == null) {
-                return minDepth(root.right) + 1;
-            } else {
-                return minDepth(root.left) + 1;
-            }
-        }
-        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
-    }
-}
+//
+//public class Solution {
+//    public int minDepth(TreeNode root) {
+//        if (root == null) return 0;
+//        if ((root.left == null) && (root.right == null)) return 1;
+//        if ((root.left == null) || (root.right == null)) {
+//            if (root.left == null) {
+//                return minDepth(root.right) + 1;
+//            } else {
+//                return minDepth(root.left) + 1;
+//            }
+//        }
+//        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+//    }
+//}
 // 精简一点
 //int min_depth = Integer.MAX_VALUE;
 //if (root.left != null) {
@@ -52,3 +52,30 @@ public class Solution {
 //        return min_depth;
 //    }
 //}
+
+//Approach 3 BFS 这样遍历到了可以立即结束节省时间
+class Solution {
+ public int minDepth(TreeNode root) {
+     LinkedList<Pair<TreeNode, Integer>> queue = new LinkedList<>();
+     if (root == null) return 0;
+     queue.add(new Pair(root,1));
+     
+     int current_depth = 0;
+     while(!queue.isEmpty()) {
+         Pair<TreeNode, Integer> current = queue.poll();
+         root = current.getKey();
+         current_depth = current.getValue();
+         if ((root.left == null) && (root.right == null)){ // is leaf
+             break;
+         }
+         if (root.left != null) {
+             queue.add(new Pair(root.left, current_depth + 1));
+         }
+         if (root.right != null) {
+             queue.add(new Pair(root.right, current_depth + 1));
+         }
+     }
+     return current_depth;
+     
+ }
+}
