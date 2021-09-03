b07de81ba138c62052d3ad726dde92ef2dd571c0
Victoria Lin <victoria.ry.lin@gmail.com>
2021-09-03 14:46:54 -0700

    LC257

--- src/LC257BinaryTreePaths/Solution.java
+++ src/LC257BinaryTreePaths/Solution.java
@@ -1,7 +1,8 @@
 package LC257BinaryTreePaths;
 
 import template.TreeNode;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
