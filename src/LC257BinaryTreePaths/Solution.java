ee4da2dfcc773ac326c7b874827f841a9b2d3d58
Victoria Lin <victoria.ry.lin@gmail.com>
2021-09-03 14:45:15 -0700

    LC 247

---
+++ src/LC257BinaryTreePaths/Solution.java
@@ -0,0 +1,35 @@
+package LC257BinaryTreePaths;
+
+import template.TreeNode;
+import java.util.*;
+
+public class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        LinkedList<String> paths = new LinkedList();
+        if (root == null) return paths;
+        
+        LinkedList<TreeNode> node_stack = new LinkedList();
+        LinkedList<String> path_stack = new LinkedList();
+        node_stack.add(root);
+        path_stack.add(Integer.toString(root.val));
+        TreeNode node;
+        String path;
+        
+        while(!node_stack.isEmpty()){
+            node = node_stack.pollLast();
+            path = path_stack.pollLast();
+            if ((node.left == null) && (node.right == null)) paths.add(path);
+            if (node.left != null) {
+                node_stack.add(node.left);
+                path_stack.add(path + "->" + Integer.toString(node.left.val));
+            }
+            if (node.right != null) {
+                node_stack.add(node.right);
+                path_stack.add(path + "->" + Integer.toString(node.right.val));
+            }
+            
+            
+        }
+        return paths;
+    }
+}
